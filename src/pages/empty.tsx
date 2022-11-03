3e9289acd54f9e36b8888b9fd954a2615e02a344
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-04 04:55:09 +1030

    Create empty.tsx

---
+++ src/pages/empty.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+
+const EmptyPage: NextPage = () => {
+
+    return (
+        <>
+            <Head>
+                <title>empty</title>
+            </Head>
+
+            <h1>empty</h1>
+
+            <h2>nothing</h2>
+
+            <div>
+                nowhere
+            </div>
+        </>
+    )
+
+}
+
+export default EmptyPage
