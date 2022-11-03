1b037750e5db5292a27e378c7a92ebdd9540be52
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-04 07:45:09 +1030

    backend checks for protected, premium, admin

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,47 +1,73 @@
-import { initTRPC, TRPCError } from "@trpc/server";
-import superjson from "superjson";
-import { useUser } from "@auth0/nextjs-auth0";
+import { initTRPC, TRPCError } from "@trpc/server"
+import superjson from "superjson"
 
-import { type Context } from "./context";
+import { type Context } from "./context"
 
 
 const t = initTRPC.context<Context>().create({
     transformer: superjson,
     errorFormatter({ shape }) {
-        return shape;
+        return shape
     },
-});
+})
 
-export const router = t.router;
+export const router = t.router
 
-/**
- * Unprotected procedure
- **/
-export const publicProcedure = t.procedure;
 
-/**
- * Reusable middleware to ensure
- * users are logged in
- */
-const isAuthed = t.middleware(({ ctx, next }) => {
-    // const { user, error, isLoading } = useUser()
-
-    console.log(`isAuthed: ctx.session: ${JSON.stringify(ctx.session, null, 4)}`);
-
-    // console.log(`isAuthed: user: ${JSON.stringify(user, null, 4)}`);
+const isRegistered = t.middleware(({ ctx, next }) => {
+    console.log(`isAuthed: ctx.session: ${JSON.stringify(ctx.session, null, 4)}`)
 
     if (!ctx.session || !ctx.session.user) {
-        throw new TRPCError({ code: "UNAUTHORIZED" });
+        throw new TRPCError({ code: "UNAUTHORIZED" })
     }
+
     return next({
         ctx: {
             // infers the `session` as non-nullable
             session: { ...ctx.session, user: ctx.session.user },
         },
-    });
-});
+    })
+})
 
-/**
- * Protected procedure
- **/
-export const protectedProcedure = t.procedure.use(isAuthed);
+
+const isPremium = t.middleware(({ ctx, next }) => {
+    console.log(`isAuthed: ctx.session: ${JSON.stringify(ctx.session, null, 4)}`)
+
+    if (!ctx.session || !ctx.session.user || !ctx.session.user.premium) {
+        throw new TRPCError({ code: "UNAUTHORIZED" })
+    }
+
+    return next({
+        ctx: {
+            // infers the `session` as non-nullable
+            session: { ...ctx.session, user: ctx.session.user },
+        },
+    })
+})
+
+
+const isAdmin = t.middleware(({ ctx, next }) => {
+    console.log(`isAuthed: ctx.session: ${JSON.stringify(ctx.session, null, 4)}`)
+
+    if (!ctx.session || !ctx.session.user || !ctx.session.user.admin) {
+        throw new TRPCError({ code: "UNAUTHORIZED" })
+    }
+
+    return next({
+        ctx: {
+            // infers the `session` as non-nullable
+            session: { ...ctx.session, user: ctx.session.user },
+        },
+    })
+})
+
+
+
+export const publicProcedure = t.procedure
+
+export const protectedProcedure = t.procedure.use(isRegistered)
+
+export const premiumProcedure = t.procedure.use(isPremium)
+
+export const adminProcedure = t.procedure.use(isAdmin)
+
