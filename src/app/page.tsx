e7151c3ff446792a0d13ef0396275fb06dba5498
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-06 10:19:32 +1030

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+'use client';
+
+// TODO: 20221106 - causing runtime error, undefined 'call', use <a> for now
+import Link from "next/link"
+
 function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
             <p>Some content</p>
+            <p>
+                <a href="/about">
+                    a link
+                </a>
+                <Link href="/about">
+                    Link link
+                </Link>
+
+            </p>
         </div>
     )
 }
