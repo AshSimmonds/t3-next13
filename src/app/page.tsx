a507079d5e9cc335f64dad9f9047283188d71af0
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-06 08:46:12 +1030

    add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-export default function HomePage() {
+function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
             <p>Some content</p>
         </div>
-    );
+    )
 }
+
+
+
+export default HomePage
