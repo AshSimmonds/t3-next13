b9e76e124f018ec038465e427f901feea508a4a5
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-06 22:15:06 +1030

    little less crazy error reporting on unauth

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,14 +4,21 @@ import { env } from "../../../env/server.mjs";
 import { createContext } from "../../../server/trpc/context";
 import { appRouter } from "../../../server/trpc/router/_app";
 
+const showUnauthorized = false
+
 // export API handler
 export default createNextApiHandler({
-  router: appRouter,
-  createContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(`❌ tRPC failed on ${path}: ${error}`);
-        }
-      : undefined,
+    router: appRouter,
+    createContext,
+    onError:
+        env.NODE_ENV === "development"
+            ? ({ path, error }) => {
+
+                if (error.code === "UNAUTHORIZED" && !showUnauthorized) {
+                    return 
+                }
+
+                console.error(`❌ ERROR: tRPC '${path}': ${error.code}`);
+            }
+            : undefined,
 });
