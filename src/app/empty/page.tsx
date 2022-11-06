418d301a138ddca8d3832959e8730d06516ddf9f
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-06 11:13:00 +1030

    empty

---
+++ src/app/empty/page.tsx
@@ -0,0 +1,22 @@
+
+function EmptyPage() {
+
+    return (
+        <>
+            <head>
+                <title>empty</title>
+            </head>
+
+            <h1>empty</h1>
+
+            <h2>nothing</h2>
+
+            <div>
+                nowhere
+            </div>
+        </>
+    )
+
+}
+
+export default EmptyPage
